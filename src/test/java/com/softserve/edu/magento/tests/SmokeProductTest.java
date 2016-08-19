b5d009505fcc859c4c6008224b40fa841bc14f8d
yulia <5111170@gmail.com>
2016-08-19 23:30:37 +0300

    added test config

--- src/test/java/com/softserve/edu/magento/tests/SmokeProductTest.java
+++ src/test/java/com/softserve/edu/magento/tests/SmokeProductTest.java
@@ -56,9 +56,7 @@ public class SmokeProductTest extends TestBase {
 	@Test(dataProvider = "parameters")
     @ServiceReport
 	public void checkProductSaved(ApplicationSources applicationSources, IAdminUser adminUser)  {
-//Search.setStrategy(adminUser.g);
 		/* Log in and go to AddProductPage */
-		applicationSources.setSearchStrategy("SearchExplicitStrategy");
 		ApplicationAdmin applicationAdmin = ApplicationAdmin.get(applicationSources);
 		DashboardPage dashboardPage = applicationAdmin.load().successAdminLogin(adminUser);
 		ProductCatalogPage productCatalogPage = dashboardPage.gotoProductCatalogPage();

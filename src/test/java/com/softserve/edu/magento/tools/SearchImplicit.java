dc4d21201059721bb7910df2932908346a7d62c5
AndriiYaremchuk <yaremchyck@gmail.com>
2016-08-21 13:09:24 +0300

    Add new Search

--- src/test/java/com/softserve/edu/magento/tools/SearchImplicit.java
+++ src/test/java/com/softserve/edu/magento/tools/SearchImplicit.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 
 public class SearchImplicit extends ASearch {
 
-    public SearchImplicit(Application<?> application) {
+    public SearchImplicit(BrowserRepository.BrowsersList) {
         super(application);
         getWebDriver().manage().timeouts().implicitlyWait(getApplication().getApplicationSources().getImplicitTimeOut(), TimeUnit.SECONDS);
         // TODO setup waits
@@ -17,135 +17,151 @@ public class SearchImplicit extends ASearch {
         //System.out.println("***SearchImplicit");
     }
 
-    private WebElement getWebElement(By by) {
-        return this.getWebDriver().findElement(by);
-    }
-
-    private WebElement getWebElement(By by, WebElement fromWebElement) {
-        return fromWebElement.findElement(by);
-    }
-
-    private List<WebElement> getWebElements(By by) {
-        return this.getWebDriver().findElements(by);
+    @Override
+    WebElement getVisibleWebElement(Find find) {
+        WebElement result;
+        BrowserRepository
     }
 
     @Override
-    public WebElement id(String id) {
-        return getWebElement(By.id(id));
+    List<WebElement> getVisibleWebElements(Find find) {
+        return null;
     }
 
     @Override
-    public WebElement name(String name) {
-        return getWebElement(By.name(name));
+    WebElement getPresentWebElement(Find find) {
+        return null;
     }
 
-    @Override
-    public WebElement xpath(String xpath) {
-        return getWebElement(By.xpath(xpath));
-    }
+//    private WebElement getWebElement(By by) {
+//        return this.getWebDriver().findElement(by);
+//    }
+//
+//    private WebElement getWebElement(By by, WebElement fromWebElement) {
+//        return fromWebElement.findElement(by);
+//    }
+//
+//    private List<WebElement> getWebElements(By by) {
+//        return this.getWebDriver().findElements(by);
+//    }
 
-    @Override
-    public WebElement cssSelector(String cssSelector) {
-        return getWebElement(By.cssSelector(cssSelector));
-    }
-
-    @Override
-    public WebElement className(String className) {
-        return getWebElement(By.className(className));
-    }
-    
-    @Override
-    public  WebElement partialLinkText(String partialLinkText) {
-    	return getWebElement(By.partialLinkText(partialLinkText));
-    }
-    
-    @Override
-    public  WebElement linkText(String linkText) {
-    	return getWebElement(By.linkText(linkText));
-    }
-    
-    @Override
-    public  WebElement tagName(String tagName) {
-    	return getWebElement(By.tagName(tagName));
-    }
-    
-    // From Elements
-    
-    @Override
-    public  WebElement id(String id, WebElement fromWebElement) {
-        return getWebElement(By.id(id), fromWebElement);
-    }
-    
-    @Override
-    public  WebElement name(String name, WebElement fromWebElement) {
-    	return getWebElement(By.name(name), fromWebElement);
-    }
-    
-    @Override
-    public  WebElement xpath(String xpath, WebElement fromWebElement) {
-    	return getWebElement(By.xpath(xpath), fromWebElement);
-    }
-    @Override
-    public  WebElement cssSelector(String cssSelector, WebElement fromWebElement) {
-    	return getWebElement(By.cssSelector(cssSelector), fromWebElement);
-    }
-    @Override
-    public  WebElement className(String className, WebElement fromWebElement) {
-    	return getWebElement(By.className(className), fromWebElement);
-    }
-    @Override
-    public  WebElement partialLinkText(String partialLinkText, WebElement fromWebElement) {
-    	return getWebElement(By.partialLinkText(partialLinkText), fromWebElement);
-    }
-    @Override
-    public  WebElement linkText(String linkText, WebElement fromWebElement) {
-    	return getWebElement(By.linkText(linkText), fromWebElement);
-    }
-    @Override
-    public  WebElement tagName(String tagName, WebElement fromWebElement) {
-    	return getWebElement(By.tagName(tagName),fromWebElement);
-    }
-    
-    // List
-    
-    @Override
-    public  List<WebElement> ids(String id) {
-    	return getWebElements(By.id(id));
-    }
-
-    @Override
-    public List<WebElement> names(String name) {
-        return getWebElements(By.name(name));
-    }
-    
-    @Override
-    public  List<WebElement> xpaths(String xpath) {
-        return getWebElements(By.xpath(xpath));
-    }
-    
-    @Override
-    public  List<WebElement> cssSelectors(String cssSelector) {
-        return getWebElements(By.cssSelector(cssSelector));
-    }
-    
-    @Override
-    public  List<WebElement> classNames(String className) {
-        return getWebElements(By.className(className));
-    }
-
-    @Override
-    public  List<WebElement> partialLinkTexts(String partialLinkText) {
-        return getWebElements(By.partialLinkText(partialLinkText));
-    }
-    
-    @Override  
-    public  List<WebElement> linkTexts(String linkText) {
-        return getWebElements(By.linkText(linkText));
-    }
-    
-    @Override
-    public  List<WebElement> tagNames(String tagName) {
-    	return getWebElements(By.tagName(tagName));
-    }
+//    @Override
+//    public WebElement id(String id) {
+//        return getWebElement(By.id(id));
+//    }
+//
+//    @Override
+//    public WebElement name(String name) {
+//        return getWebElement(By.name(name));
+//    }
+//
+//    @Override
+//    public WebElement xpath(String xpath) {
+//        return getWebElement(By.xpath(xpath));
+//    }
+//
+//    @Override
+//    public WebElement cssSelector(String cssSelector) {
+//        return getWebElement(By.cssSelector(cssSelector));
+//    }
+//
+//    @Override
+//    public WebElement className(String className) {
+//        return getWebElement(By.className(className));
+//    }
+//
+//    @Override
+//    public  WebElement partialLinkText(String partialLinkText) {
+//    	return getWebElement(By.partialLinkText(partialLinkText));
+//    }
+//
+//    @Override
+//    public  WebElement linkText(String linkText) {
+//    	return getWebElement(By.linkText(linkText));
+//    }
+//
+//    @Override
+//    public  WebElement tagName(String tagName) {
+//    	return getWebElement(By.tagName(tagName));
+//    }
+//
+//    // From Elements
+//
+//    @Override
+//    public  WebElement id(String id, WebElement fromWebElement) {
+//        return getWebElement(By.id(id), fromWebElement);
+//    }
+//
+//    @Override
+//    public  WebElement name(String name, WebElement fromWebElement) {
+//    	return getWebElement(By.name(name), fromWebElement);
+//    }
+//
+//    @Override
+//    public  WebElement xpath(String xpath, WebElement fromWebElement) {
+//    	return getWebElement(By.xpath(xpath), fromWebElement);
+//    }
+//    @Override
+//    public  WebElement cssSelector(String cssSelector, WebElement fromWebElement) {
+//    	return getWebElement(By.cssSelector(cssSelector), fromWebElement);
+//    }
+//    @Override
+//    public  WebElement className(String className, WebElement fromWebElement) {
+//    	return getWebElement(By.className(className), fromWebElement);
+//    }
+//    @Override
+//    public  WebElement partialLinkText(String partialLinkText, WebElement fromWebElement) {
+//    	return getWebElement(By.partialLinkText(partialLinkText), fromWebElement);
+//    }
+//    @Override
+//    public  WebElement linkText(String linkText, WebElement fromWebElement) {
+//    	return getWebElement(By.linkText(linkText), fromWebElement);
+//    }
+//    @Override
+//    public  WebElement tagName(String tagName, WebElement fromWebElement) {
+//    	return getWebElement(By.tagName(tagName),fromWebElement);
+//    }
+//
+//    // List
+//
+//    @Override
+//    public  List<WebElement> ids(String id) {
+//    	return getWebElements(By.id(id));
+//    }
+//
+//    @Override
+//    public List<WebElement> names(String name) {
+//        return getWebElements(By.name(name));
+//    }
+//
+//    @Override
+//    public  List<WebElement> xpaths(String xpath) {
+//        return getWebElements(By.xpath(xpath));
+//    }
+//
+//    @Override
+//    public  List<WebElement> cssSelectors(String cssSelector) {
+//        return getWebElements(By.cssSelector(cssSelector));
+//    }
+//
+//    @Override
+//    public  List<WebElement> classNames(String className) {
+//        return getWebElements(By.className(className));
+//    }
+//
+//    @Override
+//    public  List<WebElement> partialLinkTexts(String partialLinkText) {
+//        return getWebElements(By.partialLinkText(partialLinkText));
+//    }
+//
+//    @Override
+//    public  List<WebElement> linkTexts(String linkText) {
+//        return getWebElements(By.linkText(linkText));
+//    }
+//
+//    @Override
+//    public  List<WebElement> tagNames(String tagName) {
+//    	return getWebElements(By.tagName(tagName));
+//    }
 
 }

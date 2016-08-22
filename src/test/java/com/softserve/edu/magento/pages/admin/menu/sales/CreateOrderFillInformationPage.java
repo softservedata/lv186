39071c8f8bf73ee8eca802dbb25f127301957575
bodya <ravenshadow6667@gmail.com>
2016-08-22 21:57:39 +0300

    edited orders page

--- src/test/java/com/softserve/edu/magento/pages/admin/menu/sales/CreateOrderFillInformationPage.java
+++ src/test/java/com/softserve/edu/magento/pages/admin/menu/sales/CreateOrderFillInformationPage.java
@@ -16,11 +16,21 @@ public class CreateOrderFillInformationPage {
         public final WebElement searchById;
         public final WebElement addProductsToOrder;
 
-        ProductsComponent(){
+        public ProductsComponent() {
             this.listOfProducts = Search.cssSelector("#sales_order_create_search_grid_table > tbody");
             this.searchByProductName = Search.cssSelector("#sales_order_create_search_grid_filter_name");
             this.searchById = Search.cssSelector("#sales_order_create_search_grid_filter_entity_id");
             this.addProductsToOrder = Search.cssSelector("[onclick='order.productGridAddSelected()']");
+        }
+    }
+
+    private class ItemsOrderedComponent {
+        public final WebElement updateItems;
+        public final WebElement tableOfProducts;
+
+        public ItemsOrderedComponent() {
+            this.updateItems = Search.cssSelector("[onclick='order.itemsUpdate()']");
+            this.tableOfProducts = Search.cssSelector("#order-items_grid > table");
         }
     }
 
